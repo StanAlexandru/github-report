618de5da7cce122ac8143ae1b8c49f677c97be72
Alex JBM <alex_jbm@yahoo.com>
2021-01-16 14:27:51 +0200

    Added GithubService

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
+import {GithubApiService} from './GithubApiService';
+import { User } from './User';
+
 console.log('Hello!');
+
+let svc = new GithubApiService();
+svc.getUserInfo('mojodna', (user: User) => {
+    console.log(user);
+});
 
 //https://github.com/StanAlexandru/github-report.git
